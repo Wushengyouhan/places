8a614eabe828cd6128994433a8c6a6f0cd6e2b0b
Wushengyouhan <840835067@qq.com>
2024-12-16 20:42:56 +0800

    238. Extracting Code & Improving Code Structure

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import ErrorPage from "./Error.jsx";
 import Places from "./Places.jsx";
 import { sortPlacesByDistance } from "../loc";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -14,15 +15,10 @@ export default function AvailablePlaces({ onSelectPlace }) {
     async function fetchPlaces() {
       setIsFetching(true);
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
-
-        if (!response.ok) {
-          throw new Error("Faild to fetch places");
-        }
+        const places = await fetchAvailablePlaces();
         // 模拟获取经纬度的延迟
         setTimeout(() => {
-          const sortedPlaces = sortPlacesByDistance(resData.places, 30, 150);
+          const sortedPlaces = sortPlacesByDistance(places, 30, 150);
           setAvailablePlaces(sortedPlaces);
           setIsFetching(false);
         }, 2000);
